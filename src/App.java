d2295b9b47c28ae8be5028b40f0de46a704830e2
CarterFalkenberg <ckfalk21@gmail.com>
2022-02-10 11:00:39 -0500

    beginning to fill skeleton with code. End of work session

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int min = 1;
+        int max = 10;
+        int rows, cols;
+
+        /*
+        * GET ROW # and GET COL #. MUST BE INT BETWEEN MIN AND MAX (INCLUSIVE)
+        */
+       
+        // get row # 
+        System.out.println("How many rows would you like?. Minimum: " + min + ", Maximum: " + max);
+            // collect the number from the user
+        
+        // get col #
+        System.out.println("How many columns would you like?. Minimum: " + min + ", Maximum: " + max);
+            // collect the number from the user
+
+        // get contents of the matrix, row by row
+
+        // display reduced matrix
+
     }
 }
--- src/Matrix.java
+++ src/Matrix.java
@@ -1,15 +1,28 @@
 public class Matrix {
   
-  // store the contents of the matrix
-  // store the number of rows
-  // store the number of columns
+  private int[][] matrix;
+  private int rows, cols;
 
   // constructor to initialize rows, cols of the matrix
+  Matrix(int rows, int cols){
+    this.rows = rows;
+    this.cols = cols;
+  }
+
 
   // method(s) to create the matrix, row by row
+  public void createByRow(){
+
+  }
 
   // method(s) to reduce the matrix
+  public void reduce(){
+
+  }
 
   // method(s) to print the matrix to the user
+  public String toString(){
+    return "";
+  }
 
 }
