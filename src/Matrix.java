66285963738fd8d4931309657bda4846555bc9c2
CarterFalkenberg <ckfalk21@gmail.com>
2022-02-10 10:55:02 -0500

    outlined basic skeleton of classes for the purpose of reducing matrices from size 1-10

---
+++ src/Matrix.java
@@ -0,0 +1,15 @@
+public class Matrix {
+  
+  // store the contents of the matrix
+  // store the number of rows
+  // store the number of columns
+
+  // constructor to initialize rows, cols of the matrix
+
+  // method(s) to create the matrix, row by row
+
+  // method(s) to reduce the matrix
+
+  // method(s) to print the matrix to the user
+
+}
