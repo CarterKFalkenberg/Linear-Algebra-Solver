479e0021b13d63acd3a3aef5c809a4ab1b1582f1
CarterFalkenberg <ckfalk21@gmail.com>
2022-02-15 10:19:38 -0500

    able to get only valid row # and col # from the user

--- src/App.java
+++ src/App.java
@@ -1,25 +1,57 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
         int min = 1;
         int max = 10;
         int rows, cols;
+        Scanner scan = new Scanner(System.in);
 
         /*
         * GET ROW # and GET COL #. MUST BE INT BETWEEN MIN AND MAX (INCLUSIVE)
         */
-       
+
         // get row # 
+        String rowStr;
         System.out.println("How many rows would you like?. Minimum: " + min + ", Maximum: " + max);
-            // collect the number from the user
+        rowStr = scan.nextLine();
+        while (!validRowCol(rowStr, min, max)){
+            System.out.println("Invalid. Please try again");
+            rowStr = scan.nextLine();
+        }
         
         // get col #
+        String colStr;
         System.out.println("How many columns would you like?. Minimum: " + min + ", Maximum: " + max);
-            // collect the number from the user
+        colStr = scan.nextLine();
+        while (!validRowCol(colStr, min, max)){
+            System.out.println("Invalid. Please try again");
+            colStr = scan.nextLine();
+        }
 
         // get contents of the matrix, row by row
 
         // display reduced matrix
 
+
+        scan.close();
+    }
+
+
+    // determines whether a string is a number
+    public static boolean validRowCol(String str, int min, int max){
+        if (str == null){
+            return false;
+        }
+        try{
+            Integer d = Integer.parseInt(str);
+            if (d < min || d > max){
+                return false;
+            }
+        } catch (NumberFormatException nfe){
+            return false;
+        }
+        return true;
     }
 }
